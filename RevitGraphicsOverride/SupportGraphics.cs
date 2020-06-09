e8c55c4dd03b7ecbf976333bdb7562ee58aaac4d
azuev <azuev@ARM48>
2020-06-09 20:50:07 +0300

    Bugfixes for Revit 2020

--- RevitGraphicsOverride/SupportGraphics.cs
+++ RevitGraphicsOverride/SupportGraphics.cs
@@ -24,76 +24,81 @@ namespace RevitGraphicsOverride
             string message = "";
 
             if (ogs.Halftone == true)
-            {
                 message += "Полутона. ";
-            }
 
             if (ogs.ProjectionLineWeight != -1)
-            {
                 message += "Толщина линии проекции. ";
-            }
 
             if (ogs.ProjectionLineColor.IsValid)
-            {
                 message += "Цвет линии проекции. ";
-            }
 
             if (ogs.ProjectionLinePatternId.IntegerValue != invalidId)
-            {
                 message += "Тип линии проекции. ";
-            }
 
+#if R2017 || R2018
             if (ogs.IsProjectionFillPatternVisible == false)
-            {
                 message += "Отключена штриховка поверхности. ";
-            }
-
+            
             if (ogs.ProjectionFillColor.IsValid)
-            {
                 message += "Цвет штриховки поверхности. ";
-            }
 
             if (ogs.ProjectionFillPatternId.IntegerValue != invalidId)
-            {
                 message += "Штриховка поверхности. ";
-            }
 
+            if (ogs.IsCutFillPatternVisible == false)
+                message += "Отключена штриховка разреза. ";
+
+            if (ogs.CutFillColor.IsValid)
+                message += "Цвет штриховки разреза. ";
+
+            if (ogs.CutFillPatternId.IntegerValue != invalidId)
+                message += "Образец штриховки разреза. ";
+
+#else
+            if (ogs.IsSurfaceForegroundPatternVisible == false)
+                message += "Отключена штриховка передней поверхности. ";
+            if (ogs.IsSurfaceBackgroundPatternVisible == false)
+                message += "Отключена штриховка задней поверхности. ";
+ 
+            if(ogs.SurfaceForegroundPatternColor.IsValid)
+                message += "Цвет штриховки передней поверхности. ";
+            if (ogs.SurfaceBackgroundPatternColor.IsValid)
+                message += "Цвет штриховки задней поверхности. ";
+
+            if(ogs.SurfaceForegroundPatternId.IntegerValue != invalidId)
+                message += "Штриховка передней поверхности. ";
+            if (ogs.SurfaceBackgroundPatternId.IntegerValue != invalidId)
+                message += "Штриховка задней поверхности. ";
+
+            if (ogs.IsCutForegroundPatternVisible == false)
+                message += "Отключена передняя штриховка разреза. ";
+            if (ogs.IsCutBackgroundPatternVisible == false)
+                message += "Отключена задняя штриховка разреза. ";
+
+            if (ogs.CutForegroundPatternColor.IsValid)
+                message += "Цвет передней штриховки разреза. ";
+            if (ogs.CutBackgroundPatternColor.IsValid)
+                message += "Цвет задней штриховки разреза. ";
+
+            if (ogs.CutForegroundPatternId.IntegerValue != invalidId)
+                message += "Образец передней штриховки разреза. ";
+            if (ogs.CutBackgroundPatternId.IntegerValue != invalidId)
+                message += "Образец передней штриховки разреза. ";
+#endif
 
             if (ogs.Transparency != 0)
-            {
                 message += "Прозрачность. ";
-            }
 
 
             if (ogs.CutLineWeight != -1)
-            {
-                message += "Вес линии разреза. ";
-            }
+                message += "Толщина линии разреза. ";
 
             if (ogs.CutLineColor.IsValid)
-            {
                 message += "Цвет линии разреза. ";
-            }
 
             if (ogs.CutLinePatternId.IntegerValue != invalidId)
-            {
                 message += "Тип линии разреза. ";
-            }
 
-            if (ogs.IsCutFillPatternVisible == false)
-            {
-                message += "Отключена штриховка разреза. ";
-            }
-
-            if (ogs.CutFillColor.IsValid)
-            {
-                message += "Цвет штриховки разреза. ";
-            }
-
-            if (ogs.CutFillPatternId.IntegerValue != invalidId)
-            {
-                message += "Образец штриховки разреза. ";
-            }
 
             return message;
         }
